14cf62dd087ef6341781b7a348e442d988cd291c
Ofek Bickel <ofek.bickel@gmail.com>
2026-03-23 06:40:04 +0200

    fixed depreciated error caused by monaco vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import monacoEditorPluginModule from "vite-plugin-monaco-editor";
-const monacoEditorPlugin = (monacoEditorPluginModule as unknown as { default: typeof monacoEditorPluginModule }).default ?? monacoEditorPluginModule;
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [
-    react(),
-    monacoEditorPlugin({
-      languageWorkers: ["editorWorkerService"],
-    }),
-  ],
+  plugins: [react()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
